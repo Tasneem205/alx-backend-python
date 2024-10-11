47d933d4f4c5d55917b1ce788f34da297ec04458
Tasneem205 <tasneemabdeltawab205@gmail.com>
2024-10-11 05:45:55 +0300

    task 9

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
 """annotate this function task"""
-from typing import List, Tuple, Any
+from typing import List, Tuple, Sequence, Iterable
 
 
-def element_length(lst: List[Any]) -> List[Tuple[Any, int]]:
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     """the function after annotation"""
     return [(i, len(i)) for i in lst]
-
