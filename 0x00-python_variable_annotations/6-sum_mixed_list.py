d25f6d89b83644cc9d46810d3bfc8d24fc17c54c
Tasneem205 <tasneemabdeltawab205@gmail.com>
2024-10-11 04:57:00 +0300

    task 6

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -2,7 +2,7 @@
 """mixed list typing"""
 from typing import List, Union
 
+
 def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """to sum ints and floats"""
     return sum(mxd_lst)
-
