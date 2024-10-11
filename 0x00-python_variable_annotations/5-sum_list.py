16d4d98612ddd55f91dc798b0f7f2863b3aececa
Tasneem205 <tasneemabdeltawab205@gmail.com>
2024-10-11 04:53:37 +0300

    task 5

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" to sum a list of floats"""
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """ to sum list of floats"""
+    return sum(input_list)
