95b4d9b13e8ecda5176815aaa5c5e2f70303b344
Tasneem205 <tasneemabdeltawab205@gmail.com>
2024-10-11 06:24:46 +0300

    task 101 doc

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -7,6 +7,7 @@ T = TypeVar('T')  # Type variable for the key
 
 def safely_get_value(dct: Mapping[Any, T], key: Any,
                      default: Union[T, None] = None) -> Union[Any, T]:
+    """return value of a key in dict"""
     if key in dct:
         return dct[key]
     else:
