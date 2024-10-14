9367635bf28b24df8da18a9b91459961559de0ee
Tasneem205 <tasneemabdeltawab205@gmail.com>
2024-10-15 00:33:28 +0300

    task 1 in 0x01

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,1 +1,17 @@
 #!/usr/bin/env python3
+"""wait n times function file"""
+import asyncio
+from typing import List
+
+
+async def wait_n(n: int, max_delay: int = 10) -> List[float]:
+    """wait random amout of time for n times"""
+    wait_random = __import__('0-basic_async_syntax').wait_random
+    arr = []
+    tasks = [wait_random(max_delay) for _ in range(n)]
+    
+    for task in asyncio.as_completed(tasks):
+        delay = await task
+        arr.append(delay)
+
+    return arr
