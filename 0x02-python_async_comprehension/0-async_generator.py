8e39c6f981128d6058531773b171977deff61ffb
Tasneem205 <tasneemabdeltawab205@gmail.com>
2024-10-16 04:55:55 +0300

    type defintion of 0-async-generator

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -4,9 +4,10 @@ task 0
 """
 import asyncio
 import random
+from typing import List
 
 
-async def async_generator():
+async def async_generator() -> List[float]:
     """generator to generate rand numbers every second"""
     for _ in range(10):
         await asyncio.sleep(1)
